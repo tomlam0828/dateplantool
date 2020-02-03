05fb2bf85b9de62146695d3c463add1945ac6384
Tom Lam <tomlam0828@gmail.com>
2020-02-03 00:32:13 -0800

    push

---
+++ src/main/java/com/dateplan/dateplaner/DateplanerApplication.java
@@ -0,0 +1,13 @@
+package com.dateplan.dateplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateplanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateplanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dateplan/dateplaner/DateplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dateplan.dateplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateplanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
